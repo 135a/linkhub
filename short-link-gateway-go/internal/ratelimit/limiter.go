a0b2970b106fe6b22ee8d4c89167959d3e2c54d6
135a <2705056589@qq.com>
2026-04-25 10:42:42 +0800

    短链接(Go+Java版)+解决了bug

---
+++ short-link-gateway-go/internal/ratelimit/limiter.go
@@ -0,0 +1,14 @@
+﻿package ratelimit
+
+import "github.com/go-redis/redis/v8"
+
+// NewLimiter returns a Limiter for the given algorithm name.
+// Currently only "token_bucket" is supported; unknown names also use token_bucket.
+func NewLimiter(algorithm string, redisClient *redis.Client) Limiter {
+	switch algorithm {
+	case "token_bucket", "":
+		return NewTokenBucketRateLimiter(redisClient)
+	default:
+		return NewTokenBucketRateLimiter(redisClient)
+	}
+}
